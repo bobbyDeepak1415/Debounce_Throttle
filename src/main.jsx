c200ca2fa4b3a163f2f9fd2ad2e16bc6c44f6028
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-23 02:06:54 +0530

    setup for debounce

--- src/main.jsx
+++ src/main.jsx
@@ -10,13 +10,35 @@
 // )
 
 const input = document.querySelector("input");
-const typed = document.getElementById("typed_Times");
-const run = document.getElementById("funcRun_Times");
+const typed = document.querySelector("#typed_Times");
+const run = document.querySelector("#funcRun_Times");
 
 const typedTimes = 0;
 const runTimes = 0;
 
+
+const myDebounce=(func,delay)=>{
+
+  let timer=0
+
+  return function(){
+    clearTimeout(timer)
+    setTimeout(()=>{
+
+    },delay)
+  }
+
+}
+
+
+
+const debounceFunc=myDebounce(()=>{
+run.innerHTML += runTimes;
+},1500)
+
+
 input.addEventListener("input", () => {
   typed.innerHTML += typedTimes;
-  run.innerHTML += runTimes;
+  debounceFunc()
+  
 });
