f4cc7d845914a813446d98d77af6d73438ec06c5
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-23 12:41:34 +0530

    debounce and throttle revised

--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,11 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// // import './index.css'
-// import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+// import './index.css'
+import App from './App.jsx'
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
 
-const button = document.querySelector("button");
-
-const clicked = document.querySelector("#clicked_Times");
-const run = document.getElementById("funcRun_Times");
-
-let clickedTimes = 0;
-let runTimes = 0;
-
-const myDebounce = (func, interval) => {
-  let timer = 0;
-
-  return function (...args) {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      func(...args);
-    }, interval);
-  };
-};
-
-const debounceFunc = myDebounce(() => {
-  run.innerHTML = ++runTimes;
-}, 1500);
-
-button.addEventListener("click", () => {
-  clicked.innerHTML = ++clickedTimes;
-  debounceFunc();
-});
