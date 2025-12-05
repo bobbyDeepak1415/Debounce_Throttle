9eafefd000ed6bf96fce263429e74bc6987deb70
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-05 23:21:58 +0530

    changes made

--- src/practice.jsx
+++ src/practice.jsx
@@ -5,7 +5,23 @@ const funcRun = document.getElementById("funcRun_Times");
 let clickedTimes = 0;
 let runTimes = 0;
 
+const myDebounce = (func, delay) => {
+  let timer = 0;
+
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+};
+
+const debounceFunc = myDebounce(() => {
+  funcRun.innerHTML = ++runTimes;
+}, 800);
+
 button.addEventListener("click", () => {
   clicked.innerHTML = ++clickedTimes;
-  funcRun.innerHTML = ++runTimes;
+
+  debounceFunc();
 });
