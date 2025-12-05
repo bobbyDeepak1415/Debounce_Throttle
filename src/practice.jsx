1f73c2a8823b3078e99d5b08f277d7bbccc2177e
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-05 23:13:59 +0530

    changes made

--- src/practice.jsx
+++ src/practice.jsx
@@ -1,3 +1,11 @@
-const button=document.querySelector("button")
-const clicked=document.getElementById("clicked_Times")
-const funcRun = document.querySelector("funcRun_Times");
+const button = document.querySelector("button");
+const clicked = document.getElementById("clicked_Times");
+const funcRun = document.getElementById("funcRun_Times");
+
+let clickedTimes = 0;
+let runTimes = 0;
+
+button.addEventListener("click", () => {
+  clicked.innerHTML = ++clickedTimes;
+  funcRun.innerHTML = ++runTimes;
+});
