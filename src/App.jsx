5b4a6d3fa00246d305d93b400dea46ad13af7f05
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-06 23:16:48 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
   const [input, setInput] = useState("");
 
+  const [userNames, setUserNames] = useState([]);
 
+  const fetchData = async () => {
+    const response = await axios.get("https://dummyjson.com/users");
 
-  
+    const result = await response.data.users;
 
+    setUserNames(result.map((user) => user.firstName));
+
+    console.log(result.map((user) => user.firstName));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const handleClick = () => {
     alert(input);
