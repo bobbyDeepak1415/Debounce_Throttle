b910ec84aeb8d65fe7d70247826a2b5c15f584dd
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-06 23:13:00 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
+  const [input, setInput] = useState("");
+
+
+
+  
+
+
+  const handleClick = () => {
+    alert(input);
+    setInput("");
+  };
+
   return (
     <div>
-      
+      <input value={input} onChange={(e) => setInput(e.target.value)}></input>
+      <button onClick={handleClick}>Enter</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
