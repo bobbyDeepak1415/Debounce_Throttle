97cef6f080a0e30f83fc53c76dbfccfd8646ee06
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-06 23:10:17 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,11 @@
-// import "./App.css";
+import React from 'react'
 
-import { useEffect, useState } from "react";
-import Todo from "./Todo";
-import axios from "axios";
-
-function App() {
-  const [input, setInput] = useState("");
-  const [userNames, setUserNames] = useState([]);
-
-  const fetchData = async () => {
-    try {
-      const res = await axios.get("https://dummyjson.com/users");
-      const result = res.data.users;
-
-      console.log(result);
-      setUserNames(result.map((u) => u.firstName));
-    } catch (e) {
-      console.error("failed to fetch", e);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const filteredNames = userNames.filter((name) =>
-    name.toLowerCase().startsWith(input.toLowerCase())
-  );
-
-  const handleClick = () => {
-    alert(input);
-    setInput("");
-  };
-
+const App = () => {
   return (
     <div>
-      <Todo
-        input={input}
-        handleClick={handleClick}
-        setInput={setInput}
-        filteredNames={filteredNames}
-      />
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,20 +1,11 @@
-import React from "react";
+import React from 'react'
 
-const Todo = (props) => {
-  const { input, handleClick, setInput, filteredNames } = props;
-
+const Todo = () => {
   return (
     <div>
-      <input value={input} onChange={(e) => setInput(e.target.value)}></input>
-      <button onClick={handleClick}>Click</button>
-      <div>
-        {input.length > 0 &&
-          filteredNames.map((name, id) => {
-            return <li key={id}>{name}</li>;
-          })}
-      </div>
+      
     </div>
-  );
-};
+  )
+}
 
-export default Todo;
+export default Todo
