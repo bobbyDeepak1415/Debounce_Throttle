889b7ddbb7c75af7573277a6ab1bad4487c09675
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-08 00:29:37 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 const App = () => {
   const [input, setInput] = useState([]);
 
-  const [users,setUsers]=useState([])
+  const [users, setUsers] = useState([]);
 
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("https://dummyjson.com/users");
 
-  const handleClick=()=>{
-    
-  }
+      const result = await response.data.users;
+
+      setUsers(result);
+    } catch (e) {
+      console.log("failed to fetch...", e);
+    }
+  };
+
+  const handleClick = () => {};
 
   return (
     <div>
-      <input value={input} onChange={(e)=>setInput(e.target.value)}></input>
+      <input value={input} onChange={(e) => setInput(e.target.value)}></input>
       <button onClick={handleClick}>Enter</button>
       <div>{}</div>
     </div>
