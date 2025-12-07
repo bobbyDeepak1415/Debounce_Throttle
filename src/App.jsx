498716bdf389f4270c11c3b3e8cb91281a7d583e
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-07 23:43:58 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,7 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-
-import Todo from "./Todo";
+import React from "react";
 
 const App = () => {
-  const [input, setInput] = useState("");
-
-  const [userNames, setUserNames] = useState([]);
-
-  const fetchData = async () => {
-    const response = await axios.get("https://dummyjson.com/users");
-
-    const result = await response.data.users;
-
-    setUserNames(result.map((user) => user.firstName));
-
-    console.log(result.map((user) => user.firstName));
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const filteredNames = userNames.filter((name) =>
-    name.toLowerCase().startsWith(input.toLowerCase())
-  );
-
-  const handleClick = () => {
-    alert(input);
-    setInput("");
-  };
-
-  return (
-    <div>
-      <Todo
-        handleClick={handleClick}
-        filteredNames={filteredNames}
-        input={input}
-        setInput={setInput}
-      />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
