04a6251a2ac7f5b02d15b13b2c079bb15b2eae33
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-07 23:43:26 +0530

    changes made

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,47 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
-const Todo = (props) => {
-  const { handleClick, setInput, input, filteredNames } = props;
-
-  const [localValue, setLocalValue] = useState("");
-
-  const myDebounce = (func, delay) => {
-    let timer = 0;
-
-    return function (...args) {
-      clearTimeout(timer);
-
-      timer = setTimeout(() => {
-        func(...args);
-      }, delay);
-    };
-  };
-
-  const debouncedState = myDebounce((value) => {
-    setInput(value);
-  }, 3000);
-
-  const handleChange = (e) => {
-    const value = e.target.value;
-
-    setLocalValue(value);
-
-    debouncedState(value);
-  };
-
-  return (
-    <div>
-      <input value={localValue} onChange={handleChange}></input>
-      <button onClick={handleClick}>Enter</button>
-
-      <div>
-        {input.length > 0 &&
-          filteredNames.map((name, id) => {
-            return <li key={id}>{name}</li>;
-          })}
-      </div>
-    </div>
-  );
+const Todo = () => {
+  return <div></div>;
 };
 
 export default Todo;
