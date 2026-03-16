a56c289f31185cfd6bd76a21366eaf9690a2dead
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-03-16 23:59:19 +0530

    setup done

--- src/main.jsx
+++ src/main.jsx
@@ -14,17 +14,10 @@ console.log("hello");
 function countStudents(students, sandwiches) {
   for (let i = 0; i <= students.length; i++) {
     let student = students[i];
-    for (let j = 0; j <= sandwiches.length; j++) {
-      let sandwich = sandwiches[j];
-      if (sandwich !== student) {
-        let item = students.shift();
-        students.push(item);
-        // students.splice(0, 1);
-      }
       students.shift();
       sandwiches.shift();
     }
-  }
+  
   return students.length;
 }
 
