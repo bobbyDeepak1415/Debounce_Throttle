e12e85f06146d1d57d7b53bb2c67fec46ed6864b
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-03-16 17:36:03 +0530

    setup done

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// // import './index.css'
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
 
+console.log("hello")
+
+
+function countStudents(students, sandwiches) {
+  for (let i = 0; i <= students.length; i++) {
+    let student = students[i];
+    for (let j = 0; j <= sandwiches.length; j++) {
+      let sandwich = sandwiches[j];
+      if (sandwich !== student) {
+        let item = students.shift();
+        students.push(item);
+        students.splice(0, 1);
+      }
+      students.shift();
+      sandwiches.shift();
+    }
+  }
+  return students.length;
+}
+
+console.log(countStudents([1, 1, 1, 0, 0, 1], [1, 0, 0, 0, 1, 1]));
