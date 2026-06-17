5df35a7558db40608f5eb506aac185127399fbd2
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-17 17:09:41 +0530

    setup done

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import './index.css'
 // import App from "./App.jsx";
-import Practice2 from "./Practice2.jsx";
+import Practice from "./Practice.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <Practice2/>
+    <Practice/>
   </StrictMode>,
 );
