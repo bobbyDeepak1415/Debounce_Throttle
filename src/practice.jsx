5510b576d3d57677594faa12a494281f5e635ecc
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-17 23:50:23 +0530

    setup done

--- src/practice.jsx
+++ src/practice.jsx
@@ -1,11 +1,12 @@
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
 import useFetchData from "./useFetchData";
 
 const Practice = () => {
   const [input, setInput] = useState("");
-  const [localValue, setLocalValue] = useState("");
 
-  const { products } = useFetchData("https://dummyjson.com/products?limit=20");
+  const [localValue,setLocalValue]=useState("")
+
+  const { products } = useFetchData("https://dummyjson.com/products");
 
   const filteredProducts = !input.length
     ? products
@@ -13,39 +14,15 @@ const Practice = () => {
         prod.title.toLowerCase().includes(input.toLocaleLowerCase()),
       );
 
-  const myDebounce = (func, delay) => {
-    let timer = 0;
-
-    return function (...args) {
-      clearTimeout(timer);
-      timer = setTimeout(() => {
-        func(...args);
-      }, delay);
-    };
-  };
-
-  const debounceFunc = useMemo(() => {
-  return  myDebounce((value) => {
-      setInput(value);
-    }, 1000);
-  }, []);
-
-  const handleChange = (e) => {
-    const value = e.target.value;
-    setLocalValue(value);
-
-    debounceFunc(value);
-  };
-
   return (
     <div>
       <h1>Hello</h1>
-      <input value={localValue} onChange={handleChange} />
-      <button>Search</button>
-      <ul></ul>
-      {filteredProducts.map((product) => {
-        return <li key={product.id}>{product.title}</li>;
-      })}
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
+      <ul>
+        {filteredProducts.map((prod) => {
+          return <li key={prod.id}>{prod.title}</li>;
+        })}
+      </ul>
     </div>
   );
 };
