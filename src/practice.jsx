fe49977a6404ab1111443f4eb011aba18cd78b68
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-18 03:01:10 +0530

    setup done

--- src/practice.jsx
+++ src/practice.jsx
@@ -1,52 +1,7 @@
-import React, { useMemo, useState } from "react";
-import useFetchData from "./useFetchData";
+import React from "react";
 
 const Practice = () => {
-  const [input, setInput] = useState("");
-
-  // const [localalue, setLocalValue] = useState("");
-
-  const { products } = useFetchData("https://dummyjson.com/products");
-
-  const myDebounce = (func, delay) => {
-    let timer = 0;
-
-    return function (...args) {
-      clearTimeout(timer);
-      timer = setTimeout(() => {
-        func(...args);
-      }, delay);
-    };
-  };
-
-  const debounceFunc = useMemo(() => {
-    return myDebounce((value) => {
-      setInput(value);
-    }, 1000);
-  }, []);
-
-  const handleChange = (e) => {
-    // setLocalValue(e.target.value);
-    debounceFunc(e.target.value);
-  };
-
-  const filteredProducts = !input.length
-    ? products
-    : products.filter((prod) =>
-        prod.title.toLowerCase().includes(input.toLocaleLowerCase()),
-      );
-
-  return (
-    <div>
-      <h1>Hello</h1>
-      <input value={input} onChange={handleChange} />
-      <ul>
-        {filteredProducts.map((prod) => {
-          return <li key={prod.id}>{prod.title}</li>;
-        })}
-      </ul>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Practice;
