7b9c4821ffd917f30e72aa07647cb02f49cc48a4
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-18 02:51:27 +0530

    setup done

--- src/practice.jsx
+++ src/practice.jsx
@@ -2,15 +2,27 @@ import React, { useState } from "react";
 import useFetchData from "./useFetchData";
 
 const Practice = () => {
-
-  const [input,setInput]=useState("")
+  const [input, setInput] = useState("");
 
   const { products } = useFetchData("https://dummyjson.com/products");
 
-  return <div>Hello
+  const filteredProducts = !input.length
+    ? products
+    : products.filter((prod) =>
+        prod.title.toLowerCase().includes(input.toLocaleLowerCase()),
+      );
 
-
-  </div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
+      <ul>
+        {filteredProducts.map((prod) => {
+          return <li key={prod.id}>{prod.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Practice;
