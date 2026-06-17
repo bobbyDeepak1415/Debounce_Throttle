6e63ef04625db68e40ecdf9d953348c50c0cce60
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-17 16:29:19 +0530

    setup done

--- src/practice.jsx
+++ src/practice.jsx
@@ -1,43 +1,28 @@
-import React, { useState, useEffect } from "react";
-import useFetchData from "./hooks/useFetchData";
+import React, { useState } from "react";
+import useFetchData from "./useFetchData";
 
-const Practice=()=> {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [debouncedQuery, setDebouncedQuery] = useState("");
+const Practice = () => {
+  const [input, setInput] = useState("");
 
-  // 1. Every keystroke updates 'searchTerm' instantly, but triggers this effect
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      // 2. This only runs if the user stops typing for 400ms
-      setDebouncedQuery(searchTerm);
-    }, 400);
+  const { products } = useFetchData("https://dummyjson.com/products?limit=20");
 
-    return () => clearTimeout(timer); // Reset the timer if they type again
-  }, [searchTerm]);
-
-  // 4. Hook returns the raw data array
-  const { results } = useFetchData(debouncedQuery);
+  const filteredProducts = !input.length
+    ? products
+    : products.filter((prod) =>
+        prod.title.toLowerCase().includes(input.toLocaleLowerCase()),
+      );
 
   return (
-    <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
-      <input
-        type="text"
-        placeholder="Type to search..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        style={{ width: "100%", padding: "10px", fontSize: "16px" }}
-      />
-
-      <ul style={{ marginTop: "15px" }}>
-        {results.map((product) => (
-          <li key={product.id} style={{ padding: "5px 0" }}>
-            {product.title}
-          </li>
-        ))}
-      </ul>
+    <div>
+      <h1>Hello</h1>
+      <input onChange={(e) => setInput(e.target.value)} value={input} />
+      <button>Search</button>
+      <ul></ul>
+      {filteredProducts.map((product) => {
+        return <li key={product.id}>{product.title}</li>;
+      })}
     </div>
   );
-}
+};
 
-export default Practice
-
+export default Practice;
