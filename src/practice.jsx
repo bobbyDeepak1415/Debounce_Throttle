37cc9dc99ee51be44d09f5ab3c5808a104613fea
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-06 23:20:19 +0530

    setup done

--- src/practice.jsx
+++ src/practice.jsx
@@ -1,14 +1,38 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Practice = () => {
+  const [input, setInput] = useState("");
 
-  const [input,setInput]=useState("")
-  return <div>
+  const [allProducts,setAllProducts]=useState([])
 
-  <input></input>
+  const fetchData=async()=>{
+const res = await axios.get("https://dummyjson.com/products");
 
+const response=await res.data.products
+setAllProducts(response)
+  }
 
-  </div>;
+  useEffect(()=>{
+    fetchData()
+  },[])
+
+  const handleChange=(e)=>{
+setInput(e.target.value)
+  }
+
+  return (
+    <div>
+      <input value={input} onChange={handleChange} />
+
+<ul>
+  {allProducts.map((product)=>{
+    return <li>{}</li>
+  })}
+</ul>
+
+    </div>
+  );
 };
 
 export default Practice;
