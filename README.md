543a65006f04542de3a4a6bc840b3de0afd61f4f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-07 02:36:51 +0530

    setup done

---
+++ README.md
@@ -0,0 +1,16 @@
+contructor-useState
+getDerivedStateFromProps-props
+componentDidCatch-componentDidCatch
+coponentDidMount-useEffect(()=>{
+
+},[])
+
+componentDidUpdate-useEffect(()=>{
+
+},[dependency])
+
+shouldComponentUpdate-useMemo,useCallback,React.memo
+
+componentWillUnmount-useEffect(()=>{
+return ()=>cleanupFunc
+})
--- src/practice.jsx
+++ src/practice.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Practice = () => {
-  return <div>Hell00</div>;
+  const [input, setInput] = useState("");
+
+  const [allProducts, setAllProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get("https://dummyjson.com/products");
+
+      setAllProducts(res.data.products);
+    };
+
+    fetchData();
+  }, []);
+
+  const filteredProducts = input.length
+    ? allProducts.filter((product) =>
+        product.title.toLowerCase().includes(input.toLowerCase()),
+      )
+    : allProducts;
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  return (
+    <div>
+      <input value={input} onChange={handleChange} />
+      <div>
+        <ul>
+          {filteredProducts.map((product) => {
+            return <li key={product.id}>{product.title}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Practice;
