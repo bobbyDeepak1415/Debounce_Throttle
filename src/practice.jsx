466fbccdddc9918d8abf240e344574b110f3fd97
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-06 23:25:47 +0530

    setup done

--- src/practice.jsx
+++ src/practice.jsx
@@ -4,33 +4,38 @@ import React, { useEffect, useState } from "react";
 const Practice = () => {
   const [input, setInput] = useState("");
 
-  const [allProducts,setAllProducts]=useState([])
+  const [allProducts, setAllProducts] = useState([]);
 
-  const fetchData=async()=>{
-const res = await axios.get("https://dummyjson.com/products");
+  const fetchData = async () => {
+    const res = await axios.get("https://dummyjson.com/products");
 
-const response=await res.data.products
-setAllProducts(response)
-  }
+    const response = await res.data.products;
+    setAllProducts(response);
+  };
 
-  useEffect(()=>{
-    fetchData()
-  },[])
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  const handleChange=(e)=>{
-setInput(e.target.value)
-  }
+  const filteredProducts = input.length
+    ? allProducts.filter((product) =>
+        product.toLowerCase().startsWith(input.toLowerCase()),
+      )
+    : allProducts;
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
 
   return (
     <div>
       <input value={input} onChange={handleChange} />
 
-<ul>
-  {allProducts.map((product)=>{
-    return <li>{}</li>
-  })}
-</ul>
-
+      <ul>
+        {filteredProducts.map((product) => {
+          return <li key={product.id}>{product.title}</li>;
+        })}
+      </ul>
     </div>
   );
 };
