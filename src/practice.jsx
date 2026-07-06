a51c5e212e6d29e3e1691a5f18cb57de77f9c600
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-07 02:50:52 +0530

    setup done

--- src/practice.jsx
+++ src/practice.jsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useMemo } from "react";
 
 const Practice = () => {
+  const [input, setInput] = useState("");
+
+  const [allProducts, setAllProducts] = useState([]);
+
+  const [localValue, setLocalValue] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get("https://dummyjson.com/users");
+
+      const userNames = res.data.users;
+
+      setAllProducts(userNames.map((name) => name.firstName));
+    };
+
+    fetchData();
+  }, []);
+
+  const myDebounce = (func, delay) => {
+    let timer = 0;
+
+    return function (...args) {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        func(...args);
+      }, delay);
+    };
+  };
+
+  const debounceFunc = useMemo(() => {
+    return myDebounce((value) => {
+      setInput(value);
+    }, 1000);
+  }, []);
+
+  const handleChange = (e) => {
+    setLocalValue(e.target.value);
+
+    debounceFunc(e.target.value);
+  };
+
+  const filteredProducts = input.length
+    ? allProducts.filter((product) =>
+        product.toLowerCase().includes(input.toLowerCase()),
+      )
+    : allProducts;
+
   return (
     <div>
-      Hello000
+      <input value={localValue} onChange={handleChange} />
+      <div>
+        <ul>
+          {filteredProducts.map((user, id) => {
+            return <li key={id}>{user}</li>;
+          })}
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Practice
+export default Practice;
