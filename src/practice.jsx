411f9f8a4a8bb523ae21ebf98da5c05f97eae817
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-07 02:40:07 +0530

    setup done

--- src/practice.jsx
+++ src/practice.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 const Practice = () => {
   const [input, setInput] = useState("");
 
   const [allProducts, setAllProducts] = useState([]);
+
+  const [localValue, setLocalValue] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,13 +24,32 @@ const Practice = () => {
       )
     : allProducts;
 
+  const myDebounce = (func, delay) => {
+    let timer = 0;
+
+    return function (...args) {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        func(...args);
+      }, delay);
+    };
+  };
+
+  const debounceFunc = useMemo(() => {
+    return myDebounce((value) => {
+      setInput(value);
+    }, 1000);
+  }, []);
+
   const handleChange = (e) => {
-    setInput(e.target.value);
+    setLocalValue(e.target.value);
+
+    debounceFunc(e.target.value);
   };
 
   return (
     <div>
-      <input value={input} onChange={handleChange} />
+      <input value={localValue} onChange={handleChange} />
       <div>
         <ul>
           {filteredProducts.map((product) => {
