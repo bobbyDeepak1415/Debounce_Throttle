b1a7be02349a4955c95f00d468e998af13c3e620
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-18 02:08:49 +0530

    setup done

--- src/practice.jsx
+++ src/practice.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Practice = () => {
-  return <div>Hello000</div>;
+
+  const [userNames,setUserNames]=useState([])
+
+  return <div>
+    <h2>UserNames List:</h2>
+
+    
+  </div>;
 };
 
 export default Practice;
