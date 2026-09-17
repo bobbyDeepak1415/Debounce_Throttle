200aa492f5d6fd58101b8b73646f08b69c491f9c
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-18 02:16:02 +0530

    setup done

--- src/practice.jsx
+++ src/practice.jsx
@@ -1,13 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Practice = () => {
 
   const [userNames,setUserNames]=useState([])
 
+
+  useEffect(()=>{
+
+    const fetchData=async()=>{
+      try{
+
+        const res=await fetch("https://dummyjson.com/users")
+        const response=await res.json()
+        console.log(response.users)
+
+        // const firstNames=response.map((user)=>{
+        //   return {
+        //     name:user.firstName
+        //   }
+        // })
+
+        setUserNames(response.users)
+      }catch(err){
+        console.log("failed to fetch...",err)
+      }
+    }
+
+    // fetchData()
+  })
+
   return <div>
     <h2>UserNames List:</h2>
+<ul>
 
-    
+    {userNames.map((user)=>{
+      return <li>{user.name}</li>
+    })}
+    </ul>
+
+
   </div>;
 };
 
