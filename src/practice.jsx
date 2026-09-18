ca73019f0deb39123c4ff9d184b75a3b7b192d06
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-18 17:52:19 +0530

    setup done

--- src/practice.jsx
+++ src/practice.jsx
@@ -6,7 +6,6 @@ const Practice = () => {
 
   const [query,setQuery]=useState("")
 
-  console.log(userNames)
 
   useEffect(()=>{
 
@@ -15,14 +14,13 @@ const Practice = () => {
 
         const res=await fetch("https://dummyjson.com/users")
         const response=await res.json()
-        // console.log(response.users)
 
-        const firstNames=response.users.map((user)=>{
+        const firstNames=response.map((user)=>{
           return {
-            name:user.firstName
+            name:user.firstNAME
           }
         })
-
+        
         setUserNames(firstNames)
       }catch(err){
         console.log("failed to fetch...",err)
@@ -32,20 +30,25 @@ const Practice = () => {
     fetchData()
   },[])
 
-  // console.log(userNames)
 
-  const filteredNames=userNames.filter((user)=>user.name.toLowerCase().includes(query.toLowerCase()))
+  const filteredNames=userNames.filter((user)=>user.firstName.toLowerCase().includes(query.toLowerCase()))
 
   return <div style={{height:"100vh",width:"100vw",backgroundColor:"gray"}}>
     <h2>UserNames List:</h2>
 
     <input value={query} onChange={(e)=>setQuery(e.target.value)}/>
+
+    {!query? (
+<p>Search for user</p>
+    ):(
 <ul>
 
-    {userNames.map((user)=>{
+    {filteredNames.map((user)=>{
       return <li key={user.id}>{user.name}</li>
     })}
     </ul>
+
+    )}
 
 
   </div>;
