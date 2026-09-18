5e715398cbaa5aa2c735f1755975bf6a6e61ba40
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-18 18:05:01 +0530

    setup done

--- src/practice.jsx
+++ src/practice.jsx
@@ -1,55 +1,13 @@
-import React, { useEffect, useState } from "react";
+
+
+import React from 'react'
 
 const Practice = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
 
-  const [userNames,setUserNames]=useState([])
-
-  const [query,setQuery]=useState("")
-
-
-  useEffect(()=>{
-
-    const fetchData=async()=>{
-      try{
-
-        const res=await fetch("https://dummyjson.com/users")
-        const response=await res.json()
-
-        const firstNames=response.map((user)=>user.firstName)
-
-        console.log(firstNames)
-        
-        setUserNames(firstNames)
-      }catch(err){
-        console.log("failed to fetch...",err)
-      }
-    }
-
-    fetchData()
-  },[])
-
-
-  const filteredNames=userNames.filter((user)=>user.firstName.toLowerCase().includes(query.toLowerCase()))
-
-  return <div style={{height:"100vh",width:"100vw",backgroundColor:"gray"}}>
-    <h2>UserNames List:</h2>
-
-    <input value={query} onChange={(e)=>setQuery(e.target.value)}/>
-
-    {!query? (
-<p>Search for user</p>
-    ):(
-<ul>
-
-    {filteredNames.map((user)=>{
-      return <li key={user.id}>{user.name}</li>
-    })}
-    </ul>
-
-    )}
-
-
-  </div>;
-};
-
-export default Practice;
+export default Practice
