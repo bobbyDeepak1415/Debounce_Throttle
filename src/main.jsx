6783164a13e8a3b9102ef50aa3b73a923c194fba
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-20 14:59:33 +0530

    setup done

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,20 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-// import './index.css'
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// // import './index.css'
 // import App from "./App.jsx";
-import Practice from "./Practice.jsx";
+// import Practice from "./Practice.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    {/* <App /> */}
-    <Practice/>
-  </StrictMode>,
-);
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <App />
+//     {/* <Practice/> */}
+//   </StrictMode>,
+// );
 
-// console.log("1");
+console.log("1");
 
-// setTimeout(() => console.log("2"), 0);
+setTimeout(() => console.log("2"), 0);
 
-// Promise.resolve().then(() => console.log("3"));
+Promise.resolve().then(() => console.log("3"));
 
-// console.log("4");
-
+console.log("4");
